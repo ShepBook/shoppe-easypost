0fe8f40812ec7c63f92674d42c65443e0591ad39
Jared Koumentis <jared@koumentis.com>
2014-12-26 16:42:37 -0500

    add things to make a new settings tab

--- lib/shoppe/easypost.rb
+++ lib/shoppe/easypost.rb
@@ -1,10 +1,25 @@
 require "shoppe/easypost/version"
 require "shoppe/easypost/engine"
 
+require 'shoppe/navigation_manager'
+
 require "EasyPost"
 
 module Shoppe
   module Easypost
+
+    # Add easypost shipping tab to shoppe
+    Shoppe::NavigationManager.build(:admin_primary) do
+      add_item :orders
+      add_item :products
+      add_item :product_categories
+      add_item :delivery_services
+      add_item :tax_rates
+      add_item :users
+      add_item :countries
+      add_item :settings
+      add_item :easypost_shipping
+    end
 
     class << self
 
@@ -18,7 +33,7 @@ module Shoppe
         EasyPost::Shipment.create(
           {
             to_address: to_address,
-            from_address: from_address,
+            from_address: {company: Shoppe.settings.store_name, street1: Shoppe.settings.from_street_1, street2: Shoppe.settings.from_street_2, city: Shoppe.settings.from_city, state: Shoppe.settings.from_state, zip: Shoppe.settings.from_zipcode, phone: Shoppe.settings.from_phone},
             parcel: parcel
           }
         )
@@ -50,9 +65,18 @@ module Shoppe
         end
       end
 
+      def easypost_delivery_service_prices(shipment)
+        if delivery_required?
+        end
+      end
+
       def setup
         # Setup configuration with API key. Shipping options separated by commas.
-        Shoppe.add_settings_group :easypost, [:easypost_api_key, :handling_cost]
+        Shoppe.add_settings_group :easypost, [:easypost_api_key, :handling_cost, :from_street_1, :from_street_2, :from_city, :from_state, :from_zipcode, :from_phone]
+
+        Shoppe::Order.before_confirmation do
+
+        end
       end
     end
   end
